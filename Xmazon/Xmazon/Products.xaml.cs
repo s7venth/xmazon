33b1083aad72d1a287d16f5b2de6fc1f8ec8c0a6
s7venth <sylvain91130@hotmail.fr>
2016-02-08 23:25:09 +0100

    Ajout de liste

--- Xmazon/Xmazon/Products.xaml.cs
+++ Xmazon/Xmazon/Products.xaml.cs
@@ -9,6 +9,23 @@ namespace Xmazon
 	{
 		public Products ()
 		{
+			
+			var listView = new ListView();
+			listView.ItemsSource = new string[]{
+				"mono",
+				"monodroid",
+				"monotouch",
+				"monorail",
+				"monodevelop",
+				"monotone",
+				"monopoly",
+				"monomodal",
+				"mononucleosis"
+			};
+
+			//monochrome will not appear in the list because it was added
+			//after the list was populated.
+			//listView.ItemsSource.Add("monochrome");
 			InitializeComponent ();
 		}
 	}
--- Xmazon/Xmazon/Xmazon.cs
+++ Xmazon/Xmazon/Xmazon.cs
@@ -81,17 +81,7 @@ namespace Xmazon
 		public App ()
 		{
 			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+			MainPage = new NavigationPage(new Products());
 		}
 
 		protected override void OnStart ()
