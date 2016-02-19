d6b9637c105302a293f8ecd1b931c281b94672cf
Mohsen3861 <mohsenraic@hotmail.fr>
2016-02-19 20:18:23 +0100

    - login fix

--- Xmazon/Xmazon/Login.xaml.cs
+++ Xmazon/Xmazon/Login.xaml.cs
@@ -12,6 +12,31 @@ namespace Xmazon
 			InitializeComponent ();
 		}
 
+		async void OnLoginClicked (object sender, EventArgs e)
+		{
+			string username, password;
+			username = loginEntry.Text;
+			password = passwordEntry.Text;
+
+			if (!username.Equals("") && !password.Equals("")) {
+				/*
+				Console.WriteLine ("user infos : \n nom = {0} \n prenom = {1} \n email = {2} \n password = {3} \n password conf = {4} \n",
+					nom, prenom,email,password,passwordConf);
+*/
+
+				await Navigation.PushAsync (new Products ());
+			}
+				
+		}
+
+		async void OnInscriptionClicked (object sender, EventArgs e)
+		{
+			
+
+			await Navigation.PushAsync (new Register());
+
+
+		}
 
 	}
 }
